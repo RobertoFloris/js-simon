bd8515a0f981219d7d237cb83aedab93080c1647
RobertoFloris <florisroberto99@gmail.com>
2024-11-26 19:35:57 +0100

    [add] logic in script

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,107 @@
+//inizializzo due array vuoti
 const arrayRandom = [];
+const arrayUtente = []
 
+//richiamo la funzione che inserisce numeri univoci nell'array
 riempiArray(arrayRandom)
 
-console.log(arrayRandom)
-
+// seleziono gli span in cui dovranno apparire prima i numeri dell'array random e poi gli input
 const n1 = document.querySelector(".n1");
 const n2 = document.querySelector(".n2");
 const n3 = document.querySelector(".n3");
 const n4 = document.querySelector(".n4");
 const n5 = document.querySelector(".n5");
 
+// faccio apparire l'array random
 n1.innerText = arrayRandom[0];
-n2.innerText = arrayRandom[1]
-n3.innerText = arrayRandom[2]
-n4.innerText = arrayRandom[3]
-n5.innerText = arrayRandom[4]
+n2.innerText = arrayRandom[1];
+n3.innerText = arrayRandom[2];
+n4.innerText = arrayRandom[3];
+n5.innerText = arrayRandom[4];
 
+// seleziono tutti gli altri elementi che potranno servirmi
+const onTime = document.querySelector(".on-time");
+const timeExpired = document.querySelector(".time-expired")
+const send = document.querySelector(".send")
+const textSuccess = document.querySelector(".text-success");
+const textDanger = document.querySelector(".text-danger");
 const countdown = document.querySelector('.countdown');
+
+//il timer partirà da 10
 let seconds = 10;
 
+//faccio apparire 10 e lo decremento a 9
 countdown.innerHTML = seconds--;
 
-const intervalId = setInterval( () =>{
+const intervalId = setInterval(() => {
 
+  //faccio apparire 9
   countdown.innerHTML = seconds;
-  if(seconds === 0){
-    clearInterval(intervalId);
-    countdown.innerHTML = 'Il tempo è terminato!';
-  }
-  seconds--;
 
-},1000)
+  //logica per quando il timer arriverà a 0
+  if (seconds === 0) {
+    //interrompo il conto alla rovescia quando arrivo a 0
+    clearInterval(intervalId);
+
+    //cambio il layout
+    countdown.innerHTML = 'Il tempo è terminato!';
+    onTime.classList.add("d-none");
+    timeExpired.classList.remove("d-none");
+
+    //gli input sostituiscono il numero dell'array random in modo tale da non rendere possibile imbrogliare guardando con l'ispector gli elementi nascosti
+    n1.innerHTML = `<input type="number" min="1" max="50" class="n1" required>`;
+    n2.innerHTML = `<input type="number" min="1" max="50" class="n2" required>`;
+    n3.innerHTML = `<input type="number" min="1" max="50" class="n3" required>`;
+    n4.innerHTML = `<input type="number" min="1" max="50" class="n4" required>`;
+    n5.innerHTML = `<input type="number" min="1" max="50" class="n5" required>`;
+
+    //appare il pulsante invia
+    const send = document.querySelector('.send');
+    send.classList.remove('d-none');
+
+    //salvo gli elementi inseriti negli input nell'arrayUtente 
+    const form = document.getElementById('form');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+        //(.n1 input) perché input è figlio dello span con classe n1 etc
+        arrayUtente.push(parseInt(document.querySelector('.n1 input').value));
+        arrayUtente.push(parseInt(document.querySelector('.n2 input').value));
+        arrayUtente.push(parseInt(document.querySelector('.n3 input').value));
+        arrayUtente.push(parseInt(document.querySelector('.n4 input').value));
+        arrayUtente.push(parseInt(document.querySelector('.n5 input').value));
+
+      //caricato l'array, il pulsante invia sparisce
+      send.classList.add('d-none');
+
+
+      //logica per far apparire l'esito della partita
+      let count=0;
+      for(let i=0; i<arrayUtente.length; i++){
+        if(arrayRandom.includes(arrayUtente[i])){
+          count++;
+        }
+      }
+
+      if(count==1){
+        textSuccess.innerText = `Hai vinto! Ti sei ricordato ${count} numero`;
+        textSuccess.classList.remove("d-none");
+      } else if(count>0){
+        textSuccess.innerText = `Hai vinto! Ti sei ricordato ${count} numeri`;
+        textSuccess.classList.remove("d-none");
+      } else {
+        textDanger.innerText = "Hai perso";
+        textDanger.classList.remove("d-none");
+      }
+
+
+    });
+  }
+
+  // decremento la variabile del conto alla rovescia
+  seconds--;
+}, 1000); //1000 ms quindi ogni secondo si abbassa di uno
+
 
 
 
